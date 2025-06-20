468a1feae3ca51238ffdaf73a3c9a145745ebedc
mtume <mtume2016@gmail.com>
2025-06-20 08:55:30 +0300

    bubble sort

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,15 @@
 def bubble_sort(unsorted_list):
-    # TODO: Implement bubble sort
-    pass
+    for i in range(len(unsorted_list)-1):
+        for x in range(len(unsorted_list)-i-1):
+            if unsorted_list[x]>unsorted_list[x+1]:
+                print(f"swapping {arr[x]} with {arr[x+1]}")
+                unsorted_list[x], unsorted_list[x+1]=unsorted_list[x+1], unsorted_list[x]
+    return unsorted_list
+    
+
+
+
+
+
+    
+   
