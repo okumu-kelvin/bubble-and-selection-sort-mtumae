6272a44584cc3e970f5566ae1a33c1e2eb38ed2b
mtume <mtume2016@gmail.com>
2025-06-20 09:37:00 +0300

    bubble sort edit and selection sort

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,12 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    for i in range(len(arr)-1):
+        index = i
+        print(index)
+        for x in range(i+1, len(arr)):
+            if arr[x]<arr[i]:
+                index = x
+        minval = arr.pop(index)
+        arr.insert(i, minval)
+    return arr
+
+
